cf76dc630c9b8b8e7a10c6303674fccd8976783f
ismailkarabulut-lang <ismailkarabulut@gmail.com>
2025-11-11 02:18:07 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  distDir: '.next',
-}
+  reactStrictMode: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
