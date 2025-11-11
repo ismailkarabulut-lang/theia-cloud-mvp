a0c98997a886888996773f85df62e724cbedadf0
ismailkarabulut-lang <ismailkarabulut@gmail.com>
2025-11-12 02:17:36 +0300

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,6 +1,8 @@
+// lib/supabaseServer.ts
 import { createClient } from '@supabase/supabase-js';
+
 export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,            // URL
+  process.env.SUPABASE_SERVICE_ROLE!,               // SERVICE ROLE (server-only)
   { auth: { persistSession: false } }
 );
